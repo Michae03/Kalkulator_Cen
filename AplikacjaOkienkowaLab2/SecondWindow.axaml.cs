c2123a2ed1fbc8f87f46c4ffaf8d6c936e379ac7
michal <michal.now03@gmail.com>
2025-04-17 11:17:30 +0200

    Style nie dokonczone

--- AplikacjaOkienkowaLab2/SecondWindow.axaml.cs
+++ AplikacjaOkienkowaLab2/SecondWindow.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace AplikacjaOkienkowaLab2;
@@ -9,5 +10,10 @@ public partial class SecondWindow : Window
     public SecondWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }
